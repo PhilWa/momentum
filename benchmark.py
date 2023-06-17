260cece588c64878111bd5cf929e46137193a531
Philipp Warmer <philipp.warmer@d-one.ai>
2023-06-17 19:35:23 +0200

    first commit

---
+++ benchmark.py
@@ -0,0 +1,53 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+from datetime import datetime
+
+
+class TradingSimulator:
+    @staticmethod
+    def get_data(ticker, start_date, end_date, max_attempts=5):
+        for i in range(max_attempts):
+            df = yf.download(ticker, start=start_date, end=end_date, progress=False)
+            if not df.empty:
+                return df
+            else:
+                pass
+
+    @staticmethod
+    def get_closest_trading_day(date, trading_days):
+        closest_trading_day_index = trading_days.searchsorted(date)
+        return trading_days[closest_trading_day_index]
+
+    @staticmethod
+    def trade(ticker, buy_date, sell_date, capital, filename="trade_log.tsv"):
+        data = TradingSimulator.get_data(ticker, buy_date, sell_date)
+
+        if data is not None and not data.empty:
+            trading_days = np.sort(data.index.values)
+            buy_date = TradingSimulator.get_closest_trading_day(
+                np.datetime64(buy_date), trading_days
+            )
+            sell_date = TradingSimulator.get_closest_trading_day(
+                np.datetime64(sell_date), trading_days
+            )
+
+            buy_price = data.loc[buy_date, "Open"]
+            sell_price = data.loc[sell_date, "Close"]
+
+            quantity = capital / buy_price
+            pnl = (sell_price - buy_price) * quantity
+
+            with open(filename, "a") as f:
+                f.write(
+                    f"{datetime.now()}\t{ticker}\t'buy'\t{buy_price}\t{buy_date}\t'N/A'\n"
+                )
+                f.write(
+                    f"{datetime.now()}\t{ticker}\t'sell'\t{sell_price}\t{sell_date}\t{pnl}\n"
+                )
+
+
+# Buy and sell simulation
+buy_date = "2022-02-01"
+sell_date = "2023-03-01"
+TradingSimulator.trade("SPY", buy_date, sell_date, 100000)
---
+++ momentum.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import yfinance as yf
+import pandas_market_calendars as mcal
+from datetime import datetime, timedelta
+import numpy as np
+
+
+class TradingStrategy:
+    def __init__(self, tickers):
+        self.tickers = tickers
+        self.cash_balance = 100000
+        self.trade_log = pd.DataFrame(
+            columns=[
+                "Date",
+                "Ticker",
+                "Action",
+                "Quantity",
+                "Price",
+                "PnL",
+                "Timestamp",
+            ],
+        )
+
+    def calculate_momentum(self, ticker, date):
+        start_date = date - timedelta(days=365)
+        df = self.get_data(ticker, start_date, date)
+        return self.compute_momentum(df)
+
+    @staticmethod
+    def get_data(ticker, start_date, end_date, max_attempts=5):
+        for i in range(max_attempts):
+            df = yf.download(ticker, start=start_date, end=end_date, progress=False)
+            if not df.empty:
+                return df
+            else:
+                pass
+
+        print("Max attempts exceeded for: ", ticker, start_date, end_date)
+        return pd.DataFrame()  # return an empty DataFrame if all attempts fail
+
+    @staticmethod
+    def compute_momentum(df):
+        close_prices = df["Close"]
+        return (
+            ((close_prices[-1] - close_prices[0]) / close_prices[0])
+            if not df.empty
+            else 0
+        )
+
+    def sell_positions(self, date):
+        buy_log = self.trade_log[self.trade_log["Action"] == "BUY"]
+        buy_log_grouped = buy_log.groupby("Ticker")["Quantity"].sum()
+        sell_log = self.trade_log[self.trade_log["Action"] == "SELL"]
+        sell_log_grouped = sell_log.groupby("Ticker")["Quantity"].sum()
+        positions = buy_log_grouped.subtract(sell_log_grouped, fill_value=0)
+
+        for ticker in positions.index:
+            if positions[ticker] > 0:
+                last_buy_date = buy_log[buy_log["Ticker"] == ticker]["Date"].max()
+                if (date - last_buy_date).days >= 30:
+                    self.sell_position(ticker, date)
+
+    def sell_position(self, ticker, date):
+        df = self.get_data(ticker, date, date + timedelta(days=3))
+        if not df.empty:
+            self.execute_sell(df, ticker, date)
+
+    def execute_sell(self, df, ticker, date):
+        quantity_to_sell = (
+            self.trade_log[
+                (self.trade_log["Ticker"] == ticker)
+                & (self.trade_log["Action"] == "BUY")
+            ]["Quantity"].sum()
+            - self.trade_log[
+                (self.trade_log["Ticker"] == ticker)
+                & (self.trade_log["Action"] == "SELL")
+            ]["Quantity"].sum()
+        )
+
+        if quantity_to_sell > 0:
+            current_price = df["Close"][-1]
+            self.cash_balance += quantity_to_sell * current_price
+            buy_price = self.trade_log[
+                (self.trade_log["Ticker"] == ticker)
+                & (self.trade_log["Action"] == "BUY")
+            ]["Price"].values[0]
+            trade_pnl = (current_price - buy_price) * quantity_to_sell
+            print(
+                "🛒 Sell",
+                ticker,
+                "for 💵 ",
+                np.round(quantity_to_sell * current_price, 2),
+                "USD",
+                "| 💰 PnL: ",
+                np.round(trade_pnl, 2),
+            )
+
+            self.trade_log = self.trade_log.append(
+                {
+                    "Date": date,
+                    "Ticker": ticker,
+                    "Action": "SELL",
+                    "Quantity": np.round(quantity_to_sell, 2),
+                    "Price": np.round(current_price, 2),
+                    "PnL": np.round(trade_pnl, 2),
+                    "Timestamp": datetime.now(),
+                },
+                ignore_index=True,
+            )
+
+    def buy_positions(self, date, top_n=3):
+        tickers_momentum = self.get_tickers_momentum(date)
+        cash_per_position = self.cash_balance / 3
+        for ticker, momentum in tickers_momentum[:top_n]:
+            if momentum > 0 and self.cash_balance > 0:
+                self.buy_position(ticker, date, cash_per_position)
+
+    def get_tickers_momentum(self, date):
+        tickers_momentum = [
+            (ticker, self.calculate_momentum(ticker, date)) for ticker in self.tickers
+        ]
+        tickers_momentum.sort(key=lambda x: x[1], reverse=True)
+        print("💼 Ticker |🌀 Momentum")
+        _ = [print(i[0], "|", i[1]) for i in tickers_momentum]
+        return tickers_momentum
+
+    def buy_position(self, ticker, date, cash_per_position):
+        df = self.get_data(ticker, date, date + timedelta(days=3))
+        if not df.empty:
+            print("🛍️ Buy", ticker, " for 💵 ", np.round(cash_per_position, 2), "USD")
+            self.execute_buy(df, ticker, date, cash_per_position)
+
+    def execute_buy(self, df, ticker, date, cash_per_position):
+        current_price = df["Close"][-1]
+        quantity = cash_per_position / current_price
+        self.cash_balance -= cash_per_position
+        self.trade_log = self.trade_log.append(
+            {
+                "Date": date,
+                "Ticker": ticker,
+                "Action": "BUY",
+                "Quantity": np.round(quantity, 2),
+                "Price": np.round(current_price, 2),
+                "PnL": None,
+                "Timestamp": datetime.now(),
+            },
+            ignore_index=True,
+        )
+
+    def run(self, start_date, end_date):
+        print("💫 New run:", datetime.now())
+        print("💼 Tickers: ", self.tickers)
+        nyse = mcal.get_calendar("NYSE")
+        trading_days = nyse.schedule(start_date=start_date, end_date=end_date).index
+        for date in pd.date_range(start_date, end_date, freq="MS"):
+            closest_trading_day = self.get_closest_trading_day(date, trading_days)
+            print("🌱 New month:", date, "------")
+            print(" New day:", closest_trading_day, "------")
+            if not self.trade_log.empty:
+                self.sell_positions(closest_trading_day)
+            self.buy_positions(closest_trading_day)
+        self.save_log()
+
+    @staticmethod
+    def get_closest_trading_day(date, trading_days):
+        closest_trading_day_index = trading_days.searchsorted(date)
+        return trading_days[closest_trading_day_index]
+
+    def save_log(self):
+        if not self.trade_log.empty:
+            with open("data/trade_log.csv", "a") as f:
+                self.trade_log.to_csv(f, header=f.tell() == 0, index=False)
+
+
+tickers = [
+    "XLK",
+    "XLV",
+    "XLF",
+    "XLY",
+    "XLC",
+    "XLI",
+    "XLP",
+    "XLU",
+    "XLE",
+    "XLRE",
+    "XLB",
+]
+strategy = TradingStrategy(tickers)
+strategy.run("2022-05-01", "2023-1-31")
